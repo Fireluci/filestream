03ae4972e5bcc81a7c196b4d0dcd7053f36ef228
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-06-25 09:26:54 +0530

    Update stream_routes.py

--- FileStream/server/stream_routes.py
+++ FileStream/server/stream_routes.py
@@ -1,136 +1,188 @@
-import time
-import math
+import asyncio
 import logging
-import mimetypes
-import traceback
+import math
+import time
+
 from aiohttp import web
-from aiohttp.http_exceptions import BadStatusLine
-from FileStream.bot import multi_clients, work_loads, FileStream
-from FileStream.config import Telegram, Server
-from FileStream.server.exceptions import FIleNotFound, InvalidHash
-from FileStream import utils, StartTime, __version__
-from FileStream.utils.render_template import render_page
+from pyrogram.errors import FloodWait
 
-routes = web.RouteTableDef()
+from FileStream.server.exceptions import InvalidHash
+from FileStream.server.streamer import FileStreamer
+from FileStream.utils.custom_dl import ByteStreamer
 
-@routes.get("/status", allow_head=True)
-async def root_route_handler(_):
-    return web.json_response(
-        {
-            "server_status": "running",
-            "uptime": utils.get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + FileStream.username,
-            "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
-                )
-            ),
-            "version": __version__,
-        }
-    )
+LOGGER = logging.getLogger(__name__)
 
-@routes.get("/watch/{path}", allow_head=True)
-async def stream_handler(request: web.Request):
-    try:
-        path = request.match_info["path"]
-        return web.Response(text=await render_page(path), content_type='text/html')
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
-        pass
-
-
-@routes.get("/dl/{path}", allow_head=True)
-async def stream_handler(request: web.Request):
-    try:
-        path = request.match_info["path"]
-        return await media_streamer(request, path)
-    except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
-    except FIleNotFound as e:
-        raise web.HTTPNotFound(text=e.message)
-    except (AttributeError, BadStatusLine, ConnectionResetError):
-        pass
-    except Exception as e:
-        traceback.print_exc()
-        logging.critical(e.with_traceback(None))
-        logging.debug(traceback.format_exc())
-        raise web.HTTPInternalServerError(text=str(e))
+STREAM_SEMAPHORE = asyncio.Semaphore(15)
 
 class_cache = {}
 
-async def media_streamer(request: web.Request, db_id: str):
-    range_header = request.headers.get("Range", 0)
-    
-    index = min(work_loads, key=work_loads.get)
-    faster_client = multi_clients[index]
-    
-    if Telegram.MULTI_CLIENT:
-        logging.info(f"Client {index} is now serving {request.headers.get('X-FORWARDED-FOR',request.remote)}")
+work_loads = {}
 
-    if faster_client in class_cache:
-        tg_connect = class_cache[faster_client]
-        logging.debug(f"Using cached ByteStreamer object for client {index}")
-    else:
-        logging.debug(f"Creating new ByteStreamer object for client {index}")
-        tg_connect = utils.ByteStreamer(faster_client)
-        class_cache[faster_client] = tg_connect
-    logging.debug("before calling get_file_properties")
-    file_id = await tg_connect.get_file_properties(db_id, multi_clients)
-    logging.debug("after calling get_file_properties")
-    
-    file_size = file_id.file_size
 
-    if range_header:
-        from_bytes, until_bytes = range_header.replace("bytes=", "").split("-")
-        from_bytes = int(from_bytes)
-        until_bytes = int(until_bytes) if until_bytes else file_size - 1
-    else:
-        from_bytes = request.http_range.start or 0
-        until_bytes = (request.http_range.stop or file_size) - 1
+async def media_streamer(
+    request: web.Request,
+    db_id: str
+):
 
-    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
-        return web.Response(
-            status=416,
-            body="416: Range not satisfiable",
-            headers={"Content-Range": f"bytes */{file_size}"},
+    async with STREAM_SEMAPHORE:
+
+        range_header = request.headers.get("Range", None)
+
+        index = min(
+            work_loads,
+            key=work_loads.get
         )
 
-    chunk_size = 1024 * 1024
-    until_bytes = min(until_bytes, file_size - 1)
+        faster_client = work_loads[index]
 
-    offset = from_bytes - (from_bytes % chunk_size)
-    first_part_cut = from_bytes - offset
-    last_part_cut = until_bytes % chunk_size + 1
+        tg_connect = class_cache.get(index)
 
-    req_length = until_bytes - from_bytes + 1
-    part_count = math.ceil(until_bytes / chunk_size) - math.floor(offset / chunk_size)
-    body = tg_connect.yield_file(
-        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
-    )
+        if tg_connect is None:
 
-    mime_type = file_id.mime_type
-    file_name = utils.get_name(file_id)
-    disposition = "attachment"
+            tg_connect = ByteStreamer(
+                faster_client
+            )
 
-    if not mime_type:
-        mime_type = mimetypes.guess_type(file_name)[0] or "application/octet-stream"
+            class_cache[index] = tg_connect
 
-    # if "video/" in mime_type or "audio/" in mime_type:
-    #     disposition = "inline"
+        try:
 
-    return web.Response(
-        status=206 if range_header else 200,
-        body=body,
-        headers={
-            "Content-Type": f"{mime_type}",
-            "Content-Range": f"bytes {from_bytes}-{until_bytes}/{file_size}",
-            "Content-Length": str(req_length),
-            "Content-Disposition": f'{disposition}; filename="{file_name}"',
+            file_id = await tg_connect.get_file_properties(
+                db_id
+            )
+
+        except InvalidHash:
+
+            LOGGER.info("Invalid hash")
+
+            raise web.HTTPForbidden
+
+        except Exception as e:
+
+            LOGGER.exception(e)
+
+            raise web.HTTPInternalServerError
+
+        file_size = file_id.file_size
+
+        if range_header:
+
+            from_bytes, until_bytes = range_header.replace(
+                "bytes=",
+                ""
+            ).split("-")
+
+            from_bytes = int(from_bytes)
+
+            until_bytes = (
+                int(until_bytes)
+                if until_bytes
+                else file_size - 1
+            )
+
+        else:
+
+            from_bytes = 0
+            until_bytes = file_size - 1
+
+        if from_bytes > file_size:
+
+            return web.Response(
+                status=416,
+                text="Requested Range Not Satisfiable"
+            )
+
+        req_length = until_bytes - from_bytes
+
+        new_chunk_size = 1024 * 256
+
+        chunk_size = min(
+            new_chunk_size,
+            req_length
+        )
+
+        offset = from_bytes - (
+            from_bytes % chunk_size
+        )
+
+        first_part_cut = from_bytes - offset
+
+        last_part_cut = (
+            until_bytes % chunk_size
+        ) + 1
+
+        req_length = (
+            until_bytes
+            - from_bytes
+            + 1
+        )
+
+        headers = {
+            "Content-Type": file_id.mime_type,
             "Accept-Ranges": "bytes",
-        },
-    )
+            "Content-Length": str(req_length),
+            "Content-Range": (
+                f"bytes {from_bytes}-{until_bytes}/{file_size}"
+            ),
+            "Content-Disposition": (
+                f'inline; filename="{file_id.file_name}"'
+            ),
+        }
+
+        response = web.StreamResponse(
+            status=206 if range_header else 200,
+            headers=headers
+        )
+
+        await response.prepare(request)
+
+        part_count = math.ceil(
+            (until_bytes - offset) / chunk_size
+        )
+
+        body = tg_connect.yield_file(
+            file_id,
+            index,
+            offset,
+            first_part_cut,
+            last_part_cut,
+            part_count,
+            chunk_size
+        )
+
+        work_loads[index] += 1
+
+        try:
+
+            async for chunk in body:
+
+                await response.write(chunk)
+
+        except FloodWait as e:
+
+            LOGGER.warning(
+                f"FloodWait: {e.value}"
+            )
+
+            await asyncio.sleep(e.value)
+
+        except asyncio.CancelledError:
+
+            LOGGER.warning(
+                "Stream cancelled"
+            )
+
+        except Exception as e:
+
+            LOGGER.exception(e)
+
+        finally:
+
+            work_loads[index] -= 1
+
+            try:
+                await response.write_eof()
+            except:
+                pass
+
+        return response
