6b664cfb12ec0766fbaec8b94843791214ef2d9f
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-06-25 09:52:35 +0530

    Update __init__.py

--- FileStream/bot/__init__.py
+++ FileStream/bot/__init__.py
@@ -1,15 +1,24 @@
 from pyrogram import Client
+
 from FileStream.config import Telegram
+
+multi_clients = {}
+work_loads = {}
 
 FileStream = Client(
     "FileStreamBot",
+
     api_id=Telegram.API_ID,
     api_hash=Telegram.API_HASH,
     bot_token=Telegram.BOT_TOKEN,
 
-    workers=min(Telegram.WORKERS, 10),
+    workers=min(
+        Telegram.WORKERS,
+        10
+    ),
 
     sleep_threshold=30,
+
     max_concurrent_transmissions=5,
 
     no_updates=False,
